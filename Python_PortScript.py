c72624d21faf89726901648c044b515f70519506
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-10 10:09:45 -0400

    Create Python_PortScript.py
    Main Program

---
+++ Python_PortScript.py
@@ -0,0 +1,29 @@
+import re
+fname = "C:/Users/joshu/Documents/Delta Internship/Port Script/Special Ports/sample.log"
+# 10.38.254.2Stout_MC_10.38.254.2
+
+with open(fname, 'r', encoding =None) as file:
+  #search for unique identifier for device ID
+  substr1 = "Slot-1"
+  substr2 = ".1"
+  deviceName = ""
+  #search for unique identifier for vlan names
+  substring3 = "configure vlan"
+  substring4 = " add ports"
+  vlanName = ""
+  for line in file:
+    if substr1 in line:
+      index1 = line.find(substr1)
+      index2 = line.find(substr2)
+
+      deviceName = line[index1 + len(substr1) + 1: index2]
+      print(deviceName)
+    if substring3 in line and substring4 in line:
+      vlanName = line.rsplit(substring3)
+      vlanName.pop(0)
+      vlanNameStringFromList = vlanName[0]
+      vlanName2 = vlanNameStringFromList.rsplit(substring4)
+      vlanNameFinal = vlanName2[0]
+      print(vlanNameFinal)
+  
+#    else if()
